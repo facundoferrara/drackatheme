6fe8407c7aacd0e2e289e38b41a96810a1c0e7ab
Facundo Ferrara <facundoluisferrara@gmail.com>
2026-05-22 04:30:38 -0300

    blogpost structure rework

--- single-post.php
+++ single-post.php
@@ -6,15 +6,15 @@ get_header();
     <?php if (have_posts()) : ?>
         <?php while (have_posts()) : the_post(); ?>
             <article <?php post_class(); ?>>
-                <?php if (has_post_thumbnail()) : ?>
-                    <div class="post-featured-image">
+                <div class="post-hero">
+                    <?php if (has_post_thumbnail()) : ?>
                         <?php the_post_thumbnail('large'); ?>
-                    </div>
-                <?php endif; ?>
-                <h1><?php the_title(); ?></h1>
+                    <?php endif; ?>
+                    <h1><?php the_title(); ?></h1>
+                </div>
                 <p class="post-meta">
-                    <span class="post-author"><?php echo esc_html(get_the_author()); ?></span>
                     <time datetime="<?php echo esc_attr(get_the_date('c')); ?>"><?php echo esc_html(get_the_date()); ?></time>
+                    <span class="post-author"><?php echo esc_html(get_the_author()); ?></span>
                 </p>
                 <div class="post-content">
                     <?php the_content(); ?>
